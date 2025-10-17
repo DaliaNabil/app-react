0b16c4cf96f9e2cf4356d7dd03ec002fea408a7d
DaliaNabil <dalianabil213@gmail.com>
2025-10-17 21:21:48 +0300

    SETUP: Configure Vercel deployment and clean up files

--- src/components/HandleUpdateDelete/HandleUpdateDelete.jsx
+++ src/components/HandleUpdateDelete/HandleUpdateDelete.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Dropdown, DropdownItem, Label, Textarea, TextInput } from "flowbite-react";
+import { Button, Dropdown, DropdownItem, Label, Textarea} from "flowbite-react";
 import { useMutation, useQueryClient } from '@tanstack/react-query'; 
 import toast from 'react-hot-toast';
 import axios from 'axios';
